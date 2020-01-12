04e6511a79ff14707d500155475db77974295742
wickeddreamer <55858312+wickeddreamer@users.noreply.github.com>
2020-01-11 22:39:44 -0800

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,19 +1,6 @@
 document.getElementById("contact").addEventListener("click", function (){
     window.open('mailto:raforrester2936@gmail.com');
-
 });
-
-
 document.getElementById("tde").addEventListener("click", function (){
     alert("Sorry, I'm still working on this page, but feel free to check out my resume on Linkedin or send me an email.");
-
-
 });
-
-
-
-
-
-
-
-
