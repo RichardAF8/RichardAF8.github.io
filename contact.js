7d30262c81da5c25fe63b0e77531bdde6569be21
Richard Forrester <55858312+RichardAF8@users.noreply.github.com>
2020-10-03 00:40:05 -0700

    Delete contact.js

--- contact.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("contact").addEventListener("click", function (){
-    window.open('mailto:raforrester2936@gmail.com');
-});
-document.getElementById("tde").addEventListener("click", function (){
-    alert("Sorry, I'm still working on this page, but feel free to check out my resume on Linkedin or send me an email.");
-});
